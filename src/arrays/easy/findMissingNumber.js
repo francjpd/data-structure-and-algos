fe70afc46f0922533d5164df4a999cb4e9345eb3
francjpd <francjpd@gmail.com>
2024-05-16 23:44:32 +0200

    feat: findMissingNumber added

---
+++ src/arrays/easy/findMissingNumber.js
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest'
+
+const findMissingNumber = arr => {
+  const maximum = (arr.length * (arr.length + 1)) / 2
+  let countB = 0
+  for (let i = 0; i < arr.length; i++) {
+    countB += arr[i]
+  }
+
+  return Math.abs(maximum - countB)
+}
+
+describe('findMissingNumber', () => {
+  it('findMissingNumber', () => {
+    expect(findMissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])).toBe(8)
+  })
+})
