673d5bf8bb5b6ff4ce5323ff563f40bc05d30ba9
francjpd <francjpd@gmail.com>
2024-05-16 23:44:55 +0200

    feat: added maximumConseutive

---
+++ src/arrays/easy/maximuConsecutive.js
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest'
+
+const maximumConsecutive = arr => {
+  let max = 0
+  let counter = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      counter++
+    } else {
+      counter = 0
+    }
+    if (counter > max) {
+      max = counter
+    }
+  }
+  return max
+}
+
+describe('maximumConsecutive', () => {
+  it('maximumConsecutive', () => {
+    expect(maximumConsecutive([1, 1, 0, 1, 1, 1])).toBe(3)
+  })
+})
