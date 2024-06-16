c49fe5d722ea2af8deb224e2c9219bb17d083b51
francjpd <francjpd@gmail.com>
2024-06-16 23:18:51 +0200

    fix: reversePair493

--- src/arrays/hard/reversePair493.js
+++ src/arrays/hard/reversePair493.js
@@ -1,14 +1,9 @@
 const merge = (nums, low, mid, high) => {
-  let count = 0;
   let left = low;
   let right = mid + 1;
   const result = [];
 
   while (left <= mid && right <= high) {
-    count = count * 2;
-    if (nums[left] > nums[right] * 2) {
-      count++;
-    }
     if (nums[left] <= nums[right]) {
       result.push(nums[left]);
       left++;
@@ -18,36 +13,55 @@ const merge = (nums, low, mid, high) => {
     }
   }
 
-  while (left < mid) {
-    count = count * 2;
+  while (left <= mid) {
     result.push(nums[left]);
     left++;
   }
 
-  while (right < high) {
+  while (right <= high) {
     result.push(nums[right]);
     right++;
   }
 
-  for (let i = low; i < high; i++) {
-    nums[i] = result[i];
+  for (let i = low; i <= high; i++) {
+    nums[i] = result[i - low];
+  }
+};
+
+const countPairs = (nums, low, mid, high) => {
+  let right = mid + 1;
+  let count = 0;
+
+  for (let i = low; i <= mid; i++) {
+    while (right <= high && nums[i] > 2 * nums[right]) right++;
+    count += right - (mid + 1);
   }
   return count;
 };
-const mergeSort = (nums, low, high, max) => {
-  let newMax = max;
-  if (low >= high) return newMax;
+
+const mergeSort = (nums, low, high) => {
+  let count = 0;
+  if (low >= high) return count;
 
   const mid = Math.floor((low + high) / 2);
 
-  newMax += mergeSort(nums, low, mid, newMax);
-  newMax += mergeSort(nums, mid + 1, high, newMax);
-  newMax += merge(nums, low, mid, high);
-  return newMax;
+  count += mergeSort(nums, low, mid);
+  count += mergeSort(nums, mid + 1, high);
+  count += countPairs(nums, low, mid, high);
+  merge(nums, low, mid, high);
+  return count;
 };
 
 const reversePairs = (nums) => {
-  return mergeSort(nums, 0, nums.length, 0);
+  return mergeSort(nums, 0, nums.length - 1);
 };
 
-console.log(reversePairs([1, 3, 2, 3, 1]));
+// console.log(reversePairs([1, 3, 2, 3, 1]));
+
+console.log(reversePairs([2, 4, 3, 5, 1]));
+
+console.log(
+  reversePairs([
+    2147483647, 2147483647, -2147483647, -2147483647, -2147483647, 2147483647,
+  ]),
+);
