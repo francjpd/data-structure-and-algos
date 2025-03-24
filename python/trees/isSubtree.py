6e6c35256000190eefeae3d3a0a0571383939409
francjpd <francjpd@gmail.com>
2025-03-24 11:43:58 +0100

    add(tree): isSubtree.py

---
+++ python/trees/isSubtree.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from python.trees.isSame import TreeNode
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        if self.isSameTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def isSameTree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return self.isSameTree(root.left, subRoot.left) and self.isSameTree(
+                root.right, subRoot.right
+            )
+        return False
