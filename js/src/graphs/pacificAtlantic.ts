850596a04bc337b171616daf110c4f7673603f97
francjpd <francjpd@gmail.com>
2026-05-17 23:29:06 +0200

    add(pacificaAtlantic): few to many approach

--- js/src/graphs/pacificAtlantic.ts
+++ js/src/graphs/pacificAtlantic.ts
@@ -4,6 +4,64 @@ const DOWN = [1, 0];
 const LEFT = [0, -1];
 const DIRECTIONS = [UP, RIGHT, DOWN, LEFT];
 export function pacificAtlantic(heights: number[][]): number[][] {
+  if (!heights || heights.length === 0 || heights[0].length === 0) {
+    return [];
+  }
+  const rows = heights.length;
+  const cols = heights[0].length;
+
+  const pacificVisited = Array.from({ length: rows }, () =>
+    Array(cols).fill(false),
+  );
+  const atlanticVisited = Array.from({ length: rows }, () =>
+    Array(cols).fill(false),
+  );
+  const result = [];
+
+  const dfs = (row: number, col: number, oceanGrid: boolean[][]) => {
+    oceanGrid[row][col] = true;
+
+    for (const [drow, dcol] of DIRECTIONS) {
+      const nrow = row + drow;
+      const ncol = col + dcol;
+
+      if (
+        nrow >= 0 &&
+        nrow < rows &&
+        ncol >= 0 &&
+        ncol < cols &&
+        !oceanGrid[nrow][ncol] &&
+        heights[row][col] <= heights[nrow][ncol]
+      ) {
+        dfs(nrow, ncol, oceanGrid);
+      }
+    }
+  };
+  //
+
+  // iterate from  vertically
+  for (let col = 0; col < cols; col++) {
+    dfs(0, col, pacificVisited);
+    dfs(rows - 1, col, atlanticVisited);
+  }
+
+  // iterate horizontally
+  for (let row = 0; row < rows; row++) {
+    dfs(row, 0, pacificVisited);
+    dfs(row, cols - 1, atlanticVisited);
+  }
+
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < cols; c++) {
+      if (atlanticVisited[r][c] && pacificVisited[r][c]) {
+        result.push([r, c]);
+      }
+    }
+  }
+  return result;
+}
+
+export function pacificAtlanticBF(heights: number[][]): number[][] {
   const result: number[][] = [];
   const rows = heights.length;
   const cols = heights[0].length;
